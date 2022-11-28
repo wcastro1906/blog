2085965268535af094105a4ae4019848ea3562df
William Castro <wcastro@uabc.edu.mx>
2022-11-27 19:37:44 -0800

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from .views import (
+BlogListView,
+BlogDetailView,
+BlogCreateView,
+BlogUpdateView, # new
+BlogDeleteView,
+)
+
+urlpatterns = [
+path('post/<int:pk>/delete/', # new
+BlogDeleteView.as_view(), name='post_delete'),
+path('post/new/', BlogCreateView.as_view(), name='post_new'),
+path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+path('post/<int:pk>/edit/',
+BlogUpdateView.as_view(), name='post_edit'),
+path('', BlogListView.as_view(), name='home'),
+]
+
